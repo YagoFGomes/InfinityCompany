244c843b194aca5cb4e17c0377da0dcf85330b8b
Yago Ferreira <yago.fgomes@gmail.com>
2022-01-20 12:51:51 -0300

    add a shop page layout

---
+++ js/shop.js
@@ -0,0 +1,18 @@
+fetch("../js/layout.json")
+.then(response => {
+   return response.json();
+})
+.then(data =>{ 
+    for(i = 0; i < data.length; i++){
+        item = data[i]
+
+        template = `
+        <div class="banner-item">
+            <img src="${item.url}" alt="imagem camisa ${item.title}">
+            <p class="banner-title">${item.title}</p>
+        </div>
+        `
+        let div_content = document.querySelector('.grid-banner')
+        div_content.innerHTML += template  
+    }
+})
